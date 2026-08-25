f8d1ae1d80ece57cd9182f1286bd6d786fb73f5d
Emmanuelle Esguerra <emman.esguerra2013@gmail.com>
2026-08-25 17:24:18 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,19 @@ import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/nihongo-wheel/', // <-- IMPORTANT: replace with your repo name
+  base: '/nihongo-wheel/', // <-- GitHub repository name
+
   plugins: [
     vue(),
+
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+
+      includeAssets: [
+        'favicon.ico',
+        'apple-touch-icon.png'
+      ],
+
       manifest: {
         name: 'Vocabulary Wheel',
         short_name: 'VocabWheel',
@@ -16,12 +23,24 @@ export default defineConfig({
         theme_color: '#343a40',
         background_color: '#ffffff',
         display: 'standalone',
-        scope: '/nihongo-wheel/',
-        start_url: '/nihongo-wheel/',
+
         icons: [
-          { src: 'pwa-192x192.png', sizes: '192x192', type: 'image/png' },
-          { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png' },
-          { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' }
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
         ]
       }
     })
